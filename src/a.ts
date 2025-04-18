cf1d73535bbc8c59fb6d00d928ff5e8a13138729
Abhinandan Sah <abhinandansah222@gmail.com>
2025-04-18 20:33:52 +0530

    update package.json and package-lock.json to include ts-node dependency; enhance a.ts with additional types and structures; modify tsconfig.json target to es2024

--- src/a.ts
+++ src/a.ts
@@ -1,20 +1,52 @@
-// const x: number = 1;
-// console.log(x);
+let x: number = 1;
+let y: number = 2;
 
-type KeyInput = "up" | "down" | "left" | "right";
+let z: undefined = undefined;
+let w: bigint = 12343243432n;
+let v: null = null;
 
-enum Direction {
-    Up =1, //1
-    Down,  //2
-    left,  //3
-    Right, //4
+const n:number=1234;
+
+// non-premetive
+let arr: number[] = [1, 2, 3, 4, 5];
+
+let arr1 = ["avi", 1, true, null, undefined, 1234, 12343243432n];
+let arr2:(string | number | null | bigint | undefined)[] = ["avi", 1, null, undefined, 1234, 12343243432n];
+
+let tuple: [string, boolean, number] = ["avi", true, 1234];
+
+let obj1: {name: string, age: number, gender: string} = {
+    name: "Avi",
+    age: 22, 
+    gender: "Male"
 }
 
-function doSomething(keyPressed: Direction){
-    if(keyPressed == Direction.Up){
-
-    }
+type customer = {
+    name: string, 
+    id: string,
+    phone: number,
+    email: string,
 }
 
-doSomething(Direction.Up)
-doSomething(Direction.Down)
+let cust1: customer = {
+    name: "Avi",
+    id: "1234",
+    phone: 1234567890,
+    email: "avi@gmail.com"
+}
+
+interface admin{
+    name: string, 
+    id: string,
+    phone: number,
+    email: string,
+    role: string,
+}
+
+let emp1: admin = {
+    name: "Avi",
+    id: "1234",
+    phone: 1234567890,
+    email: "avi@gmail.com",
+    role: "admin"
+}
